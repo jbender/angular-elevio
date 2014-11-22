929848a7d0ab28c6f2022ec502d03a95af39b0a5
Jonathan Bender <jlbender@gmail.com>
2014-11-22 14:04:47 -0500

    setup npm and coffeescript watching

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+var gulp     = require('gulp')
+, coffee     = require('gulp-coffee')
+, coffeelint = require('gulp-coffeelint')
+, watch      = require('gulp-watch')
+, concat     = require('gulp-concat');
+
+gulp.task('compile-coffee', function () {
+  gulp.src([
+      './src/elevio.coffee'
+    ])
+    .pipe(coffeelint('.coffeelint'))
+    .pipe(coffeelint.reporter())
+    .pipe(coffee())
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('watch', function () {
+  watch('./src/*.coffee', function (files) {
+    gulp.start('compile-coffee');
+  });
+});
